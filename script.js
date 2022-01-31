a6d95067db0058b64a1954aabf91930ae52fd6dd
angie radtke <a.radtke@derauftritt.de>
2022-01-31 13:23:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+
+
+	  select=document.getElementById('payment_interval');
+	  const options = Array.from(select.options);
+
+	  options.forEach((option, i) => {
+      let radio = document.createElement('input');
+		  radio.setAttribute('type', 'radio');
+		  radio.setAttribute('name', 'payment[interval]');
+		  radio.setAttribute('value', option.value);
+		  document.getElementById('intervalChoice').append(radio);
+
+		  let label = document.createElement('label');
+		  label.setAttribute('for', option.text);
+		  label.textContent = option.text;
+
+		  document.getElementById('intervalChoice').append(label);
+
+	  });
+
+
+
+  });
